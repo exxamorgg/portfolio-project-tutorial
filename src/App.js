cd092f600ff5169949a9a47e1b92e7c1da34d187
Mehmet Ocal <magistechnology@ilter-MacBook-Pro.local>
2021-04-30 11:46:38 +0300

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
-import '/Users/magistechnology/portfolio/node_modules/bootstrap/dist/css/bootstrap.min.css' 
-import Navbar from "./components/Navbar"
+import '/Users/magistechnology/portfolio/node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Particals from "react-particles-js";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Navbar></Navbar>
+    <>
+      <Particals
+      className="partical-canvas"
+      params={{
+        particles:{ 
+          number:{
+            value: 30,
+            density:{
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape:{
+            type: "circle",
+            stroke: {
+              width: 19,
+              color: "#f9ab00"
+            }
+          }
+        }
+      }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
