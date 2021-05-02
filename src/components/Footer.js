8c1e1433df3e770ee93772ef450bae4e412c0088
Mehmet Ocal <magistechnology@ilter-MacBook-Pro.local>
2021-05-02 16:29:40 +0300

    footer.js updates link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
               <p>Istanbul, Turkey</p>
             </div>
             <div className="d-flex">
-              <a href="#" className="footer-nav">+90(506)484 92 95</a>
+              <a href="https://mehmetocal.netlify.app" className="footer-nav">+90(506)484 92 95</a>
             </div>
             <div className="d-flex">
               <p>ocal.mehmet@outlook.com.tr</p>
