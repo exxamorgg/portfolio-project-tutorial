e2dbc4116818ff3108c4381298509760a24b3c56
Mehmet Ocal <magistechnology@ilter-MacBook-Pro.local>
2021-05-02 17:54:46 +0300

    header.js updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div id="home" className="header-wrapper">
             <div className="main-info">
-                <h1>Test Automation - Web Development</h1>
+                <p>Test Automation & Web Development</p>
                 <Typed className="typed-text"
                 strings={["Manual Testing", "Test Automation", "Desktop App", "Web Development"]}
                 typeSpeed={40}
